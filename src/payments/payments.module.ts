677876c615ba869bad7ab052f862969122df134a
Joca Baldini <jcsbaldini@gmail.com>
2026-08-31 15:55:12 -0300

    feat(payments): wire repositories into payments module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { IdempotencyKey } from './entities/idempotency-key.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Payment, IdempotencyKey])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
