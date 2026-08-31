e545544af63a30d13e642f3cce55fdfd435ab7f3
Joca Baldini <jcsbaldini@gmail.com>
2026-08-31 15:27:09 -0300

    feat: enable global request validation, adding .env.example, updating app.module and packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DATABASE_HOST'),
+        port: Number(configService.getOrThrow<string>('DATABASE_PORT')),
+        username: configService.getOrThrow<string>('DATABASE_USER'),
+        password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
+        database: configService.getOrThrow<string>('DATABASE_NAME'),
+
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
